dba56651a43306fc29e7da35903f85dcb985fa3e
Florian L <leroyflorian87@gmail.com>
2019-11-14 17:50:23 +0100

    BankAccount_MultipleAmountWithdrawal_Decreases_Balance

--- BankAccountTests/BankAccountWithdrawalTests.cs
+++ BankAccountTests/BankAccountWithdrawalTests.cs
@@ -27,5 +27,30 @@ namespace BankAccountTests
             //Then
             Check.That(bankAccount.Balance).Equals(expectedBalance);
         }
+
+
+        [Test]
+        public void BankAccount_MultipleAmountWithdrawal_Decreases_Balance()
+        {
+            //Given
+            BankAccount bankAccount = new BankAccount();
+            Amount amount = new Amount(1000);
+            Balance oldBalance = bankAccount.Balance;
+
+            //When
+            bankAccount.Withdrawal(amount);
+            bankAccount.Withdrawal(amount);
+            bankAccount.Withdrawal(amount);
+
+            Balance expectedBalance = oldBalance
+                - amount
+                - amount
+                - amount;
+
+            //Then
+            Check.That(bankAccount.Balance).Equals(expectedBalance);
+        }
+
+
     }
 }
